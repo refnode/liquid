7c7c026507691bd6c91eaccf91486dbe9b688179
refnode <refnode@gmail.com>
2012-08-09 19:32:25 +0200

    Add unittest setup and regex patterns
    * Add unittests basic setup in tests/
    * Add regex patterns for parsing the template syntax
    
    Signed-off-by: refnode <refnode@gmail.com>

---
+++ src/pyliquid/patterns.py
@@ -0,0 +1,45 @@
+# Copyright 2011, refnode
+# All Rights Reserved
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+
+# import stdlibs
+# import thirdparty libs
+# import local libs
+
+
+FilterSeparator             = "\|"
+ArgumentSeparator           = ','
+FilterArgumentSeparator     = ':'
+VariableAttributeSeparator  = '.'
+TagStart                    = "\{\%"
+TagEnd                      = "\%\}"
+VariableSignature           = "\(?[\w\-\.\[\]]\)?"
+VariableSegment             = "[\w\-]"
+VariableStart               = "\{\{"
+VariableEnd                 = "\}\}"
+VariableIncompleteEnd       = "\}\}?"
+QuotedString                = "\"[^\"]*\"|'[^']*'"
+QuotedFragment              = "%s|(?:[^\s,\|'\"]|%s)+" % (QuotedString, QuotedString)
+StrictQuotedFragment        = "\"[^\"]+\"|'[^']+'|[^\s|:,]+"
+FirstFilterArgument         = "%s(?:%s)" % (FilterArgumentSeparator, StrictQuotedFragment)
+OtherFilterArgument         = "%s(?:%s)" % (ArgumentSeparator, StrictQuotedFragment)
+SpacelessFilter             = "^(?:'[^']+'|\"[^\"]+\"|[^'\"])*%s(?:%s)(?:%s(?:%s)*)?" % (FilterSeparator, StrictQuotedFragment, FirstFilterArgument, OtherFilterArgument)
+Expression                  = "(?:%s(?:%s)*)" % (QuotedFragment, SpacelessFilter)
+TagAttributes               = "(\w+)\s*\:\s*(%s)" % (QuotedFragment)
+AnyStartingTag              = "\{\{|\{\%"
+PartialTemplateParser       = "%s.*?%s|%s.*?%s" % (TagStart, TagEnd, VariableStart, VariableIncompleteEnd)
+TemplateParser              = "(%s|%s)" % (PartialTemplateParser, AnyStartingTag)
+VariableParser              = "\[[^\]]+\]|%s+\??" % (VariableSegment)
+
